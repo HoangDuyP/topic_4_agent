af66b1ff97a67ac844ccbf8166e5f3b574e1dd2b
Duy <83393404+HoangDuyP@users.noreply.github.com>
2026-03-30 17:20:16 +0700

    Intialize

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Net;
+using System.Net.Sockets;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://semiprotected-aubrey-undevelopmentally.ngrok-free.dev/hub")
+    .WithAutomaticReconnect()
+    .Build();
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connected to Hub");
+    var ip = Dns.GetHostEntry(Dns.GetHostName())
+        .AddressList
+        .FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork)
+        ?.ToString();
+
+    Console.WriteLine("IP: " + ip);
+    await connection.InvokeAsync("SendMessage", ip);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Connection failed: {ex.Message}");
+}
+
+Console.ReadLine();
